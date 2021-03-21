71d40738d5454a2824b130d50408dee09e1b6f4d
Joel Gaytan Mendez <71796345+JoelGaytan33@users.noreply.github.com>
2021-03-20 18:20:10 -0700

    clase blade
    blade

--- Artesanias/routes/web.php
+++ Artesanias/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('hola');});
+Route::get('/producto/{id}/{nombre}', function ($id, $nombre) {
+
+    return view('verproducto')->with('id',$id);
+});
+Route::get('/contacto',
+function(){
+    $contacto="Joel";
+    $valores=10;
+    $color="#CCC";
+    return view('contacto')
+    ->with('nombre',$contacto)
+    ->with('fondo',$color)
+    ->with('valores',$valores);;
 });
